c75f8ec5d805ce899e8479aa0ed610ba1f49a6bd
SasaKuruppuarachchi <sasa.kuruppuarachchi@gmail.com>
2024-03-13 13:27:28 +0200

    push submodules

---
+++ colcon_ws/src/isaac_diffbot_sim/launch/diffbot_spawn.launch.py
@@ -0,0 +1,127 @@
+import os
+import pathlib
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+import xacro
+
+def generate_launch_description():
+    isaac_diffbot_description_path = os.path.join(
+        get_package_share_directory('diffbot_description'))
+
+    xacro_file = os.path.join(isaac_diffbot_description_path,
+                              'robots',
+                              'diffbot.urdf.xacro')
+    urdf_path = os.path.join(isaac_diffbot_description_path, 'robots', 'diffbot.urdf')
+    # xacroをロード
+    doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
+    # xacroを展開してURDFを生成
+    robot_desc = doc.toprettyxml(indent='  ')
+    f = open(urdf_path, 'w')
+    f.write(robot_desc)
+    f.close()
+    relative_urdf_path = pathlib.Path(urdf_path).relative_to(os.getcwd())
+
+    params = {'robot_description': robot_desc}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("isaac_diffbot_sim"),
+            "config",
+            "isaac_diffbot.yaml",
+        ]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[params, robot_controllers],
+        output={
+            "stdout": "screen",
+            "stderr": "screen",
+        },
+    )
+    
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+    
+    diff_drive_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    velocity_converter = Node(
+        package='velocity_pub',
+        name='velocity_pub',
+        executable='velocity_pub',
+       remappings=[
+            ('cmd_vel_stamped', '/diff_drive_controller/cmd_vel'),
+        ],
+    )
+        
+    isaac_spawn_robot = Node(
+        package="isaac_ros2_scripts",
+        executable="spawn_robot",
+        parameters=[{'urdf_path': str(relative_urdf_path),
+                    'x' : 0.0,
+                    'y' : 0.0,
+                    'z' : 0.0,
+                    'R' : 0.0,
+                    'P' : 0.0,
+                    'Y' : 1.57,
+                    }],
+    )
+
+    isaac_prepare_sensors = Node(
+        package="isaac_ros2_scripts",
+        executable="prepare_sensors",
+        parameters=[{'urdf_path': str(relative_urdf_path)}],
+    )
+
+    isaac_prepare_robot_controller = Node(
+        package="isaac_ros2_scripts",
+        executable="prepare_robot_controller",
+        parameters=[{'urdf_path': str(relative_urdf_path)}],
+    )
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=isaac_spawn_robot,
+                on_exit=[isaac_prepare_sensors],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=isaac_prepare_sensors,
+                on_exit=[isaac_prepare_robot_controller],
+            )
+        ),
+        control_node,
+        node_robot_state_publisher,
+        joint_state_broadcaster_spawner,
+        diff_drive_controller_spawner,
+        velocity_converter,
+        isaac_spawn_robot,
+    ])
